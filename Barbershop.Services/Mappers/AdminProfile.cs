9ff3b423198b3d2ef2061d49b1f006f2d0921053
kostyaLem <kostyaLem@yandex.ru>
2024-03-08 19:03:09 +0300

    Фикс маппинга

--- Barbershop.Services/Mappers/AdminProfile.cs
+++ Barbershop.Services/Mappers/AdminProfile.cs
@@ -9,16 +9,10 @@ namespace Barbershop.Services.Mappers
     {
         public AdminProfile()
         {
+            CreateMap<User, AdminDto>();
+
             CreateMap<Admin, AdminDto>()
-                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.User.PhoneNumber))
-                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.User.FirstName))
-                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.User.LastName))
-                .ForMember(dest => dest.Surname, opt => opt.MapFrom(src => src.User.Surname))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
-                .ForMember(dest => dest.Photo, opt => opt.MapFrom(src => src.User.Photo))
-                .ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => src.User.Birthday))
-                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Login))
-                .ReverseMap();
+                .IncludeMembers(x => x.User);
 
             CreateMap<AdminDto, CreateAdminCommand>()
                 .ForMember(dest => dest.Password,
